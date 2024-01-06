46ff9d678297ac90ae36baa5ea27b1db24d041f3
Arthur Cottey <arthur.cottey1@gmail.com>
2024-01-06 21:28:26 +0100

    Add Task

--- App.js
+++ App.js
@@ -1,17 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  KeyboardAvoidingView,
+  TextInput,
+  TouchableOpacity,
+  Platform,
+  Keyboard
+} from 'react-native';
 import Task from './components/Task';
 
 export default function App() {
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([])
+
+  const handleAddTask = () => {
+    Keyboard.dismiss
+    setTaskItems([...taskItems, task])
+    setTask(null);
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.tasksWrapper}>
         <Text style={styles.sectionTitle}>Today's tasks</Text>
         <View style={styles.items}>
-          <Task text={'Task 1'} />
-          <Task text={'Task 2'} />
+          {
+            taskItems.map((item, index) => {
+              return (<Task text={item} />)
+            })
+          }
         </View>
       </View>
+
+      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={styles.writeTaskWrapper}>
+        <TextInput style={styles.input} placeholder={'Write a task'} value={task} onChangeText={text => setTask(text)}/>
+
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+
     </View>
   );
 }
@@ -31,5 +64,32 @@ const styles = StyleSheet.create({
   },
   items: {
     marginTop: 30
-  }
+  },
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 50,
+    width: '100%',
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center"
+  },
+  input: {
+    padding: 15,
+    backgroundColor: '#fff',
+    width: 250,
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1
+  },
+  addWrapper: {
+    height: 60,
+    width: 60,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: "center",
+    borderColor: '#C0C0C0',
+    borderWidth: 1
+  },
+  addText: {},
 });
